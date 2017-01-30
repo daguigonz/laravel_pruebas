e0e6595828b7760467a4c71ca85a13da5049d273
Daniel Aguirre <danielaguirre@Daguigonz.local>
2017-01-29 23:32:03 -0500

    Initial commit.

---
+++ database/migrations/2017_01_30_035523_add_pages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('page_id');
+            $table->string('page_title',140);
+            $table->string('page_slug',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
